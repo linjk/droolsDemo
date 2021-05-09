7cf5663e50df0a6de48ab7963663170be96c548c
LinJK <linjk121@163.com>
2021-05-09 16:50:28 +0800

    比较操作符 contains

---
+++ src/main/java/cn/linjk/drools/demo/entity/CompareEntity.java
@@ -0,0 +1,32 @@
+package cn.linjk.drools.demo.entity;
+
+import java.util.List;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: CompareEntity
+ * @author: linjk
+ * @date: 2021/5/9 下午4:24
+ * @description: 比较操作符学习测试
+ */
+public class CompareEntity {
+    private String names;
+    private List<String> list;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
--- src/test/java/cn/linjk/drools/demo/DroolsTest.java
+++ src/test/java/cn/linjk/drools/demo/DroolsTest.java
@@ -1,5 +1,6 @@
 package cn.linjk.drools.demo;
 
+import cn.linjk.drools.demo.entity.CompareEntity;
 import cn.linjk.drools.demo.entity.Order;
 import org.junit.Test;
 import org.kie.api.KieServices;
@@ -23,6 +24,20 @@ public class DroolsTest {
         // Fact对象
         Order order = new Order();
         order.setOriginalPrice(121.1);
+        kieSession.insert(order);
+        // 激活规则，由drools框架自动进行规则匹配
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+
+    @Test
+    public void testTwo() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
+        // Fact对象
+        CompareEntity order = new CompareEntity();
+        order.setNames("linjk");
         kieSession.insert(order);
         // 激活规则，由drools框架自动进行规则匹配
         kieSession.fireAllRules();
